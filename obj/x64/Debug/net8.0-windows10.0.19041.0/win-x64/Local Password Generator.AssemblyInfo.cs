2d8daad47322f0c57f609827e5b7261747ce6096
TyW04 <tywagner04@gmail.com>
2025-09-04 21:53:38 -0600

    Auto-generated file changes

--- obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Local Password Generator.AssemblyInfo.cs
+++ obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Local Password Generator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Local Password Generator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c87ba47f2196f24e73ed2490aa24546876d3041d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+284373a1c54d84f18b1d76bb7b48e25ad3cf02bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Local Password Generator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Local Password Generator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
