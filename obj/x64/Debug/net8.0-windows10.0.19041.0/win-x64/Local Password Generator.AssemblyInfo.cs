06d4f8507061e8d6104d670e73721ded1e7d9c4e
TyW04 <tywagner04@gmail.com>
2025-09-06 12:36:10 -0600

    Auto-updated VS Project files

--- obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Local Password Generator.AssemblyInfo.cs
+++ obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Local Password Generator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Local Password Generator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+284373a1c54d84f18b1d76bb7b48e25ad3cf02bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0fef898aa309d5c2dfab601ae25488e18587c99")]
 [assembly: System.Reflection.AssemblyProductAttribute("Local Password Generator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Local Password Generator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
